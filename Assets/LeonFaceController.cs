8ac9892ef87f33e78b7874569e0159cfe2e9b714
red-pencil <hx.4700.00@gmail.com>
2018-10-18 03:18:15 +0900

    almost done

--- Assets/LeonFaceController.cs
+++ Assets/LeonFaceController.cs
@@ -1,16 +1,234 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class LeonFaceController : MonoBehaviour {
+namespace UnityChan
+{
+	public class LeonFaceController : MonoBehaviour
+	{
+		public AnimationClip[] animations;
+		public Animator anim;
+		public float delayWeight;
+		public bool isKeepFace = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+		public static float MicLoudness;
+
+		private string _device;
+
+		// custom keys
+
+		public enum faceFuncs {eye_close_unitychan, smile1_unitychan, smile2_unitychan};
+
+		public faceFuncs faceX;
+
+		private AutoBlink autoBlinkInfo;
+		public bool _autoBlinkON;
+
+		private LeonGesture gestureInfo;
+		public bool _realBackgroundON;
+
+		public string[] faceMapping = new string[10];
+
+		public KeyCode smile;
+		public KeyCode angry;
+		public KeyCode eyeClose;
+
+
+		void Start ()
+		{
+			anim = GetComponent<Animator> ();
+			autoBlinkInfo = GetComponent<AutoBlink> ();
+			gestureInfo = GameObject.Find ("EventSystem").GetComponent<LeonGesture> ();
+
+		}
+
+		//void OnGUI ()
+		//{
+		//	GUILayout.Box ("Face Update", GUILayout.Width (170), GUILayout.Height (25 * (animations.Length + 2)));
+		//	Rect screenRect = new Rect (10, 25, 150, 25 * (animations.Length + 1));
+		//	GUILayout.BeginArea (screenRect);
+		//	foreach (var animation in animations) {
+		//		if (GUILayout.RepeatButton (animation.name)) {
+		//			anim.CrossFade (animation.name, 0);
+		//		}
+		///	}
+		//	isKeepFace = GUILayout.Toggle (isKeepFace, " Keep Face");
+		//	GUILayout.EndArea ();
+		//}
+
+		float current = 0;
+
+		bool  _isSpeaking = false;
+
+		void Update ()
+		{
+			// levelMax equals to the highest normalized value power 2, a small number because < 1
+			// pass the value to a static var so we can access it from anywhere
+			MicLoudness = LevelMax ();
+
+			if (MicLoudness > -80)
+				_isSpeaking = true;
+			else
+				_isSpeaking = false;
+
+			//Debug.Log (_isSpeaking);
+
+			if (_isSpeaking) {
+				//anim.CrossFade ("MTH_O", 0);
+			}
+				
+			if (Input.GetKey (KeyCode.KeypadEnter)) {
+				isKeepFace = true;
+			} else {
+				isKeepFace = false;
+			}
+
+
+			if (Input.GetKeyDown (KeyCode.KeypadMinus)) {
+				_autoBlinkON = !_autoBlinkON;
+			}
+
+			if (Input.GetKeyDown (KeyCode.KeypadPlus)) {
+				_realBackgroundON = !_realBackgroundON;
+			}
+
+			autoBlinkInfo.isActive = _autoBlinkON;
+			gestureInfo.realScene.SetActive (!_realBackgroundON);
+
+
+			//if (Input.GetMouseButton (0)) {
+			//	current = 1;
+			//} else if (!isKeepFace) {
+			//	current = Mathf.Lerp (current, 0, delayWeight);
+			//}
+
+			if (Input.GetKeyDown (eyeClose)) {
+
+				anim.CrossFade ("eye_close@unitychan", 0);
+
+			}
+
+			if (Input.GetKeyDown (KeyCode.Keypad1)) {
+
+				anim.CrossFade ("eye_close@unitychan", 0);
+
+			}
+
+			if (Input.GetKeyDown (KeyCode.Keypad2)) {
+
+				anim.CrossFade ("smile1@unitychan", 0);
+
+			}
+
+			if (Input.GetKeyDown (KeyCode.Keypad3)) {
+
+				anim.CrossFade ("angry1@unitychan", 0);
+
+			}
+
+			if (Input.GetKeyDown (KeyCode.F)) {
+
+				anim.CrossFade ("MTH_A", 0);
+
+			}
+
+			if (Input.anyKeyDown) {
+
+				current = 1;
+			} else if (!isKeepFace) {
+				current = Mathf.Lerp (current, 0, delayWeight);
+			}
+
+			anim.SetLayerWeight (1, current);
+		}
+
+
+		//アニメーションEvents側につける表情切り替え用イベントコール
+		public void OnCallChangeFace (string str)
+		{   
+			int ichecked = 0;
+			foreach (var animation in animations) {
+				if (str == animation.name) {
+					ChangeFace (str);
+					break;
+				} else if (ichecked <= animations.Length) {
+					ichecked++;
+				} else {
+					//str指定が間違っている時にはデフォルトで
+					str = "default@unitychan";
+					ChangeFace (str);
+				}
+			} 
+		}
+
+		void ChangeFace (string str)
+		{
+			isKeepFace = true;
+			current = 1;
+			anim.CrossFade (str, 0);
+		}
+
+
+
+		/// <summary>
+		/// VOLUME CONTROL
+		/// </summary>
+
+
+ 
+        //mic initialization
+        void InitMic(){
+			if (Microphone.devices.Length > 0) {
+				Debug.Log ("microphone ON" + Microphone.devices.Length );
+			} else {
+				Debug.Log ("microphone OFF");
+			}
+            if(_device == null) _device = Microphone.devices[0];
+			_clipRecord = Microphone.Start(_device, true, 999, 44100);
+        }
+ 
+        void StopMicrophone()
+        {
+            Microphone.End(_device);
+        } 
+ 
+        AudioClip _clipRecord = new AudioClip();
+        int _sampleWindow = 128;
+ 
+        //get data from microphone into audioclip
+        float LevelMax()
+        {
+            float levelMax = 0;
+            float[] waveData = new float[_sampleWindow];
+            int micPosition = Microphone.GetPosition(null)-(_sampleWindow+1); // null means the first microphone
+            if (micPosition < 0) return 0;
+            _clipRecord.GetData(waveData, micPosition);
+            // Getting a peak on the last 128 samples
+            for (int i = 0; i < _sampleWindow; i++) {
+                float wavePeak = waveData[i] * waveData[i];
+                if (levelMax < wavePeak) {
+                    levelMax = wavePeak;
+                }
+            }
+
+			float db = 20 * Mathf.Log10 (Mathf.Abs (levelMax));
+
+            return db;
+        }
+ 
+ 
+ 
+        bool _isInitialized;
+        // start mic when scene starts
+        void OnEnable()
+        {
+            InitMic();
+            _isInitialized=true;
+        }
+
+
+
+
 	}
 }
---
+++ Assets/LeonFaceMapping.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class LeonFaceMapping : MonoBehaviour {
+
+
+	public enum players {unitychan_player_1, unitychan_player_2};
+	public players matchedPlayer;
+
+	private UnityChan.LeonFaceController conPlayer;
+
+	public KeyCode[] faces;
+
+	//public KeyCode smile1;
+	//public KeyCode smile2;
+	//public KeyCode confuse;
+	//public KeyCode sap;
+	//public KeyCode angry1;
+	//public KeyCode angry2;
+	//public KeyCode distract1;
+	//public KeyCode distract2;
+	//public KeyCode eyeClose;
+	//public KeyCode superised;
+	//public KeyCode mouthA;
+	//public KeyCode mouthE;
+	//public KeyCode mouthI;
+	//public KeyCode mouthO;
+	//public KeyCode mouthU;
+
+
+	// Use this for initialization
+	void Start () {
+
+		string playerName = matchedPlayer.ToString();
+
+		conPlayer = GameObject.Find (playerName).GetComponent<UnityChan.LeonFaceController> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		int keyOrder;
+		foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode))) {
+			if (Input.GetKey (vKey)) {
+				Debug.Log (vKey); 
+				keyOrder = Array.IndexOf (faces, vKey);
+				Debug.Log (keyOrder);
+
+				if (keyOrder < 0) return;
+
+				conPlayer.anim.CrossFade (conPlayer.animations [keyOrder].name, 0);
+
+			}
+		}
+
+
+
+		
+	}
+}
