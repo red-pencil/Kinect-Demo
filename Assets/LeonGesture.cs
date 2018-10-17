1d10b814039677b77f6a33c3029bce8ba29b7e35
red-pencil <hx.4700.00@gmail.com>
2018-10-17 22:36:49 +0900

    Initial commit

---
+++ Assets/LeonGesture.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeonGesture : MonoBehaviour {
+
+
+	private CubeGestureListener gestureListener;
+
+	new public GameObject avatarEyeL;
+	new public GameObject avatarEyeR;
+
+	public bool _eyeChangeWithKeys;
+	public bool _eyeChangeWithGestures;
+
+	public GameObject realScene;
+	public bool _realSceneON;
+
+	// Use this for initialization
+	void Start () {
+
+		gestureListener = CubeGestureListener.Instance;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!gestureListener)
+			return;
+
+		_realSceneON = realScene.activeSelf;
+
+		if(_eyeChangeWithKeys)
+		{
+			if(Input.GetKeyDown(KeyCode.Keypad1))
+				eyeBlinkLeft();
+			else if(Input.GetKeyDown(KeyCode.Keypad3))
+				eyeBlinkRight();
+			else if(Input.GetKeyDown(KeyCode.Keypad0))
+				realScene.SetActive(!_realSceneON);
+		}
+
+		if(_eyeChangeWithGestures && gestureListener)
+		{
+			if(gestureListener.IsSwipeLeft())
+				eyeBlinkLeft();
+			else if(gestureListener.IsSwipeRight())
+				eyeBlinkRight();
+		}
+
+	}
+
+	private void eyeBlinkLeft() {
+		avatarEyeL.SetActive (!avatarEyeL.activeSelf);
+	}
+
+	private void eyeBlinkRight() {
+		avatarEyeR.SetActive (!avatarEyeR.activeSelf);
+	}
+
+}
---
+++ Assets/leonCustomController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leonCustomController : MonoBehaviour {
+	
+	new public GameObject avatarEyeL;
+	new public GameObject avatarEyeR;
+
+	public GameObject realScene;
+	public bool _realSceneON;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//_realSceneON = realScene.GetComponent<ForegroundToRawImage> ().isActiveAndEnabled;
+		_realSceneON = realScene.activeSelf;
+
+		if (Input.GetKeyDown (KeyCode.KeypadEnter)) {
+			
+			realScene.SetActive(!_realSceneON);
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+
+			avatarEyeL.SetActive (!avatarEyeL.activeSelf);
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+
+			avatarEyeR.SetActive (!avatarEyeR.activeSelf);
+		}
+
+
+	}
+}
