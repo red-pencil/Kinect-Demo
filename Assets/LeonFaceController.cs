553465b68a317bc5704979d91720ec954e89d946
red-pencil <hx.4700.00@gmail.com>
2018-10-24 16:50:42 +0900

    some bug

--- Assets/LeonFaceController.cs
+++ Assets/LeonFaceController.cs
@@ -24,6 +24,7 @@ namespace UnityChan
 		private LeonGesture gestureInfo;
 		public bool _realBackgroundON;
 
+		private OVRLipSyncMicInput mouthSyncInfo;
 		//public string[] faceMapping = new string[10];
 
 		public KeyCode smile;
@@ -36,6 +37,7 @@ namespace UnityChan
 			anim = GetComponent<Animator> ();
 			autoBlinkInfo = GetComponent<AutoBlink> ();
 			gestureInfo = GameObject.Find ("EventSystem").GetComponent<LeonGesture> ();
+			mouthSyncInfo = GetComponent<OVRLipSyncMicInput> ();
 
 		}
 
@@ -65,33 +67,17 @@ namespace UnityChan
 			// pass the value to a static var so we can access it from anywhere
 
 
-			//if (MicLoudness > -80)
-			//	_isSpeaking = true;
-			//else
-			//	_isSpeaking = false;
-
-			if (Input.GetKeyDown (KeyCode.KeypadPeriod)) _speakMode = !_speakMode;
-			
-			if (_speakMode) {
-
 				current = 0;
-				MicLoudness = LevelMax ();
+				//MicLoudness = LevelMax ();
 				//Debug.Log (MicLoudness);
 
-				if (MicLoudness > -80)
-				{
-					Debug.Log ("Speaking");
-					anim.CrossFade ("MTH_U", 0);
-					if (MicLoudness > -50 || MicLoudness < -80) {
-						anim.CrossFade ("MTH_A", 0);
-						Debug.Log ("Loudly");
-					}
-					current = 0.5f;
-				}
+			//if (MicLoudness > -50) {
 
-				anim.SetLayerWeight (1, current);
-				return;
-			}
+			//	_speakMode = true;
+
+			//} else {
+			//	_speakMode = false;
+			//}
 				
 
 			if (Input.GetKeyDown (KeyCode.KeypadMultiply)) {
@@ -106,7 +92,7 @@ namespace UnityChan
 				//anim.CrossFade ("MTH_O", 0);
 			//}
 				
-			if (Input.GetKeyDown (KeyCode.KeypadDivide))
+			if (Input.GetKeyDown (KeyCode.KeypadPeriod))
 				_longPress = !_longPress;
 
 			if (_longPress) {
@@ -119,15 +105,14 @@ namespace UnityChan
 
 			} else {
 				
-				if (Input.GetKeyDown (KeyCode.KeypadMultiply))
+				if (Input.GetKeyDown (KeyCode.KeypadPlus))
 					isKeepFace = !isKeepFace;
 
 			}
 
 
-
-			if (Input.GetKeyDown (KeyCode.KeypadMinus)) _autoBlinkON = !_autoBlinkON;
-			if (Input.GetKeyDown (KeyCode.KeypadPlus)) _realBackgroundON = !_realBackgroundON;
+			if (Input.GetKeyDown (KeyCode.KeypadMultiply)) _autoBlinkON = !_autoBlinkON;
+			if (Input.GetKeyDown (KeyCode.KeypadMinus)) _realBackgroundON = !_realBackgroundON;
 
 			autoBlinkInfo.isActive = _autoBlinkON;
 			gestureInfo.realScene.SetActive (_realBackgroundON);
@@ -239,11 +224,11 @@ namespace UnityChan
             _isInitialized=true;
         }
 
-		void OnGUI ()
-		{
-			Rect rect1 = new Rect (10, Screen.height - 40, 400, 30);
-			_speakMode = GUI.Toggle (rect1, _speakMode, "Speaking Mode");
-		}
+		//void OnGUI ()
+		//{
+		//	Rect rect1 = new Rect (10, Screen.height - 40, 400, 30);
+		//	_speakMode = GUI.Toggle (rect1, _speakMode, "Speaking Mode");
+		//}
 
 
 	}
--- Assets/Oculus/LipSync/Scripts/OVRLipSyncContextMorphTarget.cs
+++ Assets/Oculus/LipSync/Scripts/OVRLipSyncContextMorphTarget.cs
@@ -116,7 +116,7 @@ public class OVRLipSyncContextMorphTarget : MonoBehaviour
     /// <summary>
     /// Update this instance.
     /// </summary>
-    void LateUpdate ()
+    void Update ()
     {
         if((lipsyncContext != null) && (skinnedMeshRenderer != null))
         {
@@ -163,12 +163,8 @@ public class OVRLipSyncContextMorphTarget : MonoBehaviour
     {
         for (int i = 0; i < visemeToBlendTargets.Length; i++)
         {
-            //leon if (visemeToBlendTargets[i] != -1)
-			if (visemeToBlendTargets[i] != -1)
+            if (visemeToBlendTargets[i] != -1)
             {
-				// leon add
-				if (visemeToBlendTargets[i] >10) return;
-
                 // Viseme blend weights are in range of 0->1.0, we need to make range 100
                 skinnedMeshRenderer.SetBlendShapeWeight(
                     visemeToBlendTargets[i],
@@ -191,9 +187,6 @@ public class OVRLipSyncContextMorphTarget : MonoBehaviour
             laughterScore = laughterScore < laughterThreshold ? 0.0f : laughterScore - laughterThreshold;
             laughterScore = Mathf.Min(laughterScore * laughterMultiplier, 1.0f);
             laughterScore *= 1.0f / laughterThreshold;
-
-			// leon add
-			if ( laughterBlendTarget >10) return;
 
             skinnedMeshRenderer.SetBlendShapeWeight(
                 laughterBlendTarget,
